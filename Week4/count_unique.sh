5069d277b7565f7bfa1f4296c56b5335915d07ce
John Kiernan <johntk@users.noreply.github.com>
2016-02-28 14:24:23 +0000

    Update count_unique.sh

--- Week4/count_unique.sh
+++ Week4/count_unique.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #prints all the files in the passed in \dir, cuts the bits we want out, sorts them, counts uniq names
+# This script only works in a raspberry pi/ Deb
 printf "$(file $1/* | cut  -c21-50 | cut -d',' -f1 | cut -d'`' -f1 | sort | uniq -c)\n"
